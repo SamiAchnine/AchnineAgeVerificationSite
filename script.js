9474ec4d50ff1a3d36e2120e81b454a2f7c10f93
SamiAchnine <satejeda99@gmail.com>
2026-02-19 13:02:15 -0500

    completed assignment

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const output = document.getElementById("output")
 button.addEventListener("click", function() {
     let age = Number(ageInput.value);
     console.log(age);
-    if (age > 10) {
-        output.innerHTML = "grug";
+    
+    if (age > 99) {
+        output.innerHTML = "You need to be old enough to play with LEGOs to use this site, but still, good job on reaching this old age! How was 1926?";
+    }
+    else if (age >= 50) {
+        output.innerHTML = "Over halfway there... muahahahahahahaha";
+    }
+    else if (age == 42) {
+        output.innerHTML = "Please tell me the meaning of life. You're 42, you <em>MUST</em> know it.";
+    }
+    else if (age <= 25) {
+        output.innerHTML = "Your brain is not fully developed, that's not good. Go to <a href=\"https://google.com/\" target=\"_blank\">google.com</a> and get one.";
+    }
+    else if (age < 3) {
+        output.innerHTML = "You need to be old enough to play with LEGOs to use this site. Sorry!";
+    }
+    else if (isNaN(age)) {
+        output.innerHTML = "I... I don't know what to say... you... uh... happy birthday?";
     }
     else {
-        output.innerHTML = "blug";
+        output.innerHTML = "You're just an age. Nice.";
     }
 });
